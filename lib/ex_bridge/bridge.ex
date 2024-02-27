b0016e59fa22728795c9a23ddd40473d90ee4e9e
Adriano Santos <sleipnir@bsd.com.br>
2024-02-26 23:29:24 -0300

    Send command

--- lib/ex_bridge/bridge.ex
+++ lib/ex_bridge/bridge.ex
@@ -1,16 +1,56 @@
 defmodule ExBridge.Bridge do
+  @moduledoc """
+  TODO
+  """
   use GenServer
+  require Logger
 
-  @impl true
-  def ini({callback, init_opts, port_args}) do
+  @spec port :: port | nil
+  def port, do: Process.get({__MODULE__, :port})
+
+  @spec command(port, String.t(), any(), pid()) :: :ok
+  def command(port, name, args \\ [], caller \\ nil) do
+    data = serialize(name, args, caller)
+    Port.command(port, data)
+    :ok
+  end
+
+  defp serialize(name, args, caller) when not is_nil(caller) and is_pid(caller) do
+    ref =
+      :erlang.pid_to_list(caller)
+      |> to_string()
+
+    nil
+  end
+
+  @impl GenServer
+  def init({callback, init_opts, port_args}) do
     Process.flag(:trap_exit, true)
 
     port = open(init_opts, port_args)
+    Process.put({__MODULE__, :port}, port)
 
     {:ok, %{}}
   end
 
-  defp port(init_opts, port_args) do
+  @impl GenServer
+  def terminate(reason, state) do
+    if port() != nil, do: close()
+  end
+
+  @impl GenServer
+  def handle_info({port, {:exit_status, status}}, state) when is_port(port) do
+    Logger.error("Unexpected port exit with status #{status}")
+    Process.delete({__MODULE__, :port})
+    {:stop, :port_crash, state}
+  end
+
+  @impl GenServer
+  def handle_info({port, {:data, msg}}, state) when is_port(port) do
+    {:noreply, state}
+  end
+
+  defp open(init_opts, port_args) do
     Port.open(
       {:spawn_executable, System.find_executable(init_opts.executable)},
       [
@@ -21,5 +61,17 @@ defmodule ExBridge.Bridge do
         args: init_opts.arguments
       ]
     )
+  end
+
+  defp close do
+    port = port()
+    # command(port, :stop)
+
+    receive do
+      {^port, {:exit_status, 0}} -> :ok
+      {^port, {:exit_status, status}} -> {:error, status}
+    after
+      :timer.seconds(5) -> {:error, :timeout}
+    end
   end
 end
